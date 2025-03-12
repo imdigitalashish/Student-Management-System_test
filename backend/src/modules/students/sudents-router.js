f7fd1c4d802076d166d508cb3f2e3906abd48695
imdigitalashish <imdigitalashish@gmail.com>
2025-03-12 18:32:53 +0530

    Fixed Notice Page, Added CRUD, Fixed Filter Creteria as well, with Adaptability with Frontend and Search Also FIxed

--- backend/src/modules/students/sudents-router.js
+++ backend/src/modules/students/sudents-router.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const studentController = require("./students-controller");
 
-router.get("", studentController.handleGetAllStudents);
+router.get("/", studentController.handleGetAllStudents);
 router.post("", studentController.handleAddStudent);
 router.get("/:id", studentController.handleGetStudentDetail);
 router.post("/:id/status", studentController.handleStudentStatus);
